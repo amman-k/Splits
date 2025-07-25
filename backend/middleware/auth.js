6da0514d52b5d3ee73fc0972af84ed1543e5b077
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-07-25 12:16:03 +0530

    solved  auth issue

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,21 @@
-const jwt=require("jsonwebtoken");
+// backend/middleware/auth.js
+
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-module.exports= function(req,res,next){
-    const token=req.header('x-auth-header');
-    if(!token){
-        return res.status(401).json({msg:'No token,authorization denied'});
-    }
+module.exports = function (req, res, next) {
 
-    try{
-        const decoded= jwt.verify(token,process.env.JWT_SECRET);
-        req.user=decoded.user;
-        next();
-    }catch(err){
-        res.status(401).json({msg:'Token is not valid'});
-    }
+  const token = req.header('x-auth-token');
 
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
 
-}
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
