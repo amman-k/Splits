6ded5de1d9bf79082aa458c686c0e5de3fcb844e
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-07-25 21:12:56 +0530

    separated private and public routes

--- frontend/splits-app/src/App.jsx
+++ frontend/splits-app/src/App.jsx
@@ -1,19 +1,26 @@
-import React from "react";
-import {Routes,Route} from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
-import GroupDetailsPage from './pages/GroupDetailsPage';
+import GroupDetailPage from './pages/GroupDetailsPage';
 
-function App(){
-  return(
+
+import PrivateRoute from './components/routing/PrivateRoute';
+
+function App() {
+  return (
     <Routes>
-      <Route path="/login" element={<LoginPage/>}></Route>
-      <Route path="/register" element={<RegisterPage/>}></Route>
-      <Route path="/dashboard" element={<DashboardPage/>}></Route>
-      <Route path="/group/id:" element={<GroupDetailsPage/>}></Route>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/" element={<LoginPage />} />
+
+      <Route element={<PrivateRoute />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/group/:id" element={<GroupDetailPage />} />
+      </Route>
     </Routes>
   );
 }
